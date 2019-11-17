9ff37b6cf303a011a12798a38999ed0a18364983
genshiro-dtb <57723860+genshiro-dtb@users.noreply.github.com>
2019-11-17 22:32:13 +0100

    Add files via upload

---
+++ Armor penetration/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim v As Double
+    Dim p As Double
+    Dim k As Double
+    Dim d As Double
+    Dim b As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        v = Val(TextBox1.Text)
+        p = Val(TextBox2.Text)
+        k = Val(TextBox3.Text)
+        d = Val(TextBox4.Text)
+
+        b = (v * p ^ (1 / 2)) / (k * d ^ (1 / 2))
+        TextBox5.Text = b * 1000
+    End Sub
+
+
+
+
+End Class
